76068cc4c457fa70bfc53734d915f66bf85d517f
inkwolf42 <inkwolf42@gmail.com>
2024-11-03 14:08:42 +0100

    Second commitgit add .

---
+++ main.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define STRING_MAX_LENGTH 30
+#define BLOCK_MAX_SIZE 30
+
+typedef struct S{
+   char id[STRING_MAX_LENGTH];
+   char Fname[STRING_MAX_LENGTH];
+   char Lname[STRING_MAX_LENGTH];
+   int  group;
+   float Note;
+   struct S *next;
+}TStudent;
+
+typedef struct node{
+   TStudent val;
+   struct node *next;
+   int length;
+}TBlock;
+
+typedef struct{
+   int NumberOfBlocks;
+   int NumberOfRecordes;
+}THeader;
+
+THeader Header(FILE *f){
+   fseek(f,0,SEEK_SET);
+   THeader h;
+   fread(&h,sizeof(THeader),1,f);
+   return h;
+}
+
+void SetHeader(FILE *f,THeader *h){
+   fseek(f,0,SEEK_SET);
+   fwrite(h,sizeof(THeader),1,f);
+}
+
+
+
+int main(){
+
+   
+
+   return 0;
+}
